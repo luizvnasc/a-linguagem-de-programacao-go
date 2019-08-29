a15c13e287a21765584bd0042eb659826ebd2c44
Guto <luiz.v.nasc@gmail.com>
2019-08-29 16:55:07 -0300

    Implementado os exercicios 1.1 e 1.2

--- ch1/echo3/main.go
+++ ch1/echo3/main.go
@@ -7,5 +7,14 @@ import (
 )
 
 func main() {
+
+	//echo3 original
 	fmt.Println(strings.Join(os.Args[1:], " "))
+	//exercicio 1.1
+	fmt.Println(strings.Join(os.Args, " "))
+	//exercicio 1.2
+	for i, arg := range os.Args {
+		fmt.Printf("%d: %s\n", i, arg)
+	}
+
 }

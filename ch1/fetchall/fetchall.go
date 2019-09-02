37a94b02bb49450805dc66370a0b2bbf00f62af2
Guto <luiz.v.nasc@gmail.com>
2019-09-02 16:02:54 -0300

    Implementado o fetchall

---
+++ ch1/fetchall/fetchall.go
@@ -0,0 +1,40 @@
+// Fetchall busca URLs em paralelo e informa os tempos gastos e os tamanhos.
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // inicia uma go rotina
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // recebe do canal ch
+	}
+	fmt.Printf("%.2f elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) //envia para o canal ch
+		return
+	}
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v\n", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}

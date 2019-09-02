8483511e06bc4c7403605baa5b7fd04d650871f0
Guto <luiz.v.nasc@gmail.com>
2019-09-02 14:18:38 -0300

    Implementados os exercicios 1.7,1.8,1.9

--- ch1/fetch/fetch.go
+++ ch1/fetch/fetch.go
@@ -3,25 +3,31 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+		fmt.Printf("Status: %s\n\nBody:\n\n", resp.Status)
+		_, err = io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s\n", b)
+
+		//fmt.Printf("Status: %s\n\n%s\n", resp.Status, )
 
 	}
 }
